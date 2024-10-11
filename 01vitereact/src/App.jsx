286f88a12840fd90eb827771c6c758548b0d0884
shailjasoni2005 <144000431+shailjasoni2005@users.noreply.github.com>
2024-10-11 18:11:51 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "./react"
 function App() {
+  const username = "react code."
 
   return (
     <>
     <React/>
-    <h1>react with vite</h1>
+    <h1>react with vite with {username}</h1>
+    {/* this username is evaluated expression and here we always writes evaluated expression that has been evaluated from js already. */}
     </>
-  )
+  ) 
 }
 
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,42 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+function MyApp(){
+  return(
+    <div>
+      <h1>custom app !</h1>
+    </div>
+  )
+}
+
+/*const ReactElement = {
+  type: 'a',
+  props: {
+      href: 'https://google.com',
+      target: '_blank'
+  },
+  children: 'click me to visit google'
+}*/
+
+
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherUser = "chai aur react";
+
+// creating react element according to react
+const reactElement = React.createElement(
+  // here the syntax is predefined for creating element 
+  'a',             //tag
+  {href: 'https://google.com' , target: '_blank' },   //object
+  'click me to visit google',
+  anotherElement
+)
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  reactElement
 )
